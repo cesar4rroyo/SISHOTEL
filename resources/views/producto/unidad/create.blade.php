efb6e18718994d55c4571e0bf61f04830963490f
Cesar <cesardavid_09@hotmail.com>
2020-12-04 12:32:30 -0500

    finish unidad crud

--- resources/views/producto/unidad/create.blade.php
+++ resources/views/producto/unidad/create.blade.php
@@ -1,36 +1,36 @@
-@extends('layouts.app')
+@extends("theme.$theme.layout")
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            @include('admin.sidebar')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Crear Nueva unidad</div>
+                <div class="card-body">
+                    <a href="{{ route('unidad') }}" title="Back"><button class="btn btn-warning btn-sm"><i
+                                class="fa fa-arrow-left" aria-hidden="true"></i> Regresar</button></a>
+                    <br />
+                    <br />
 
-            <div class="col-md-9">
-                <div class="card">
-                    <div class="card-header">Create New unidad</div>
-                    <div class="card-body">
-                        <a href="{{ url('/unidad') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-                        <br />
-                        <br />
+                    @if ($errors->any())
+                    <ul class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                    @endif
 
-                        @if ($errors->any())
-                            <ul class="alert alert-danger">
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        @endif
+                    <form method="POST" action="{{ route('store_unidad') }}" accept-charset="UTF-8"
+                        class="form-horizontal" enctype="multipart/form-data">
+                        {{ csrf_field() }}
 
-                        <form method="POST" action="{{ url('/unidad') }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
-                            {{ csrf_field() }}
+                        @include ('producto.unidad.form', ['formMode' => 'create'])
 
-                            @include ('producto.unidad.form', ['formMode' => 'create'])
+                    </form>
 
-                        </form>
-
-                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
--- resources/views/producto/unidad/show.blade.php
+++ resources/views/producto/unidad/show.blade.php
@@ -1,40 +1,47 @@
-@extends('layouts.app')
-
+@extends("theme.$theme.layout")
 @section('content')
-    <div class="container">
-        <div class="row">
-            @include('admin.sidebar')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Unidad {{ $unidad->id }}</div>
+                <div class="card-body">
 
-            <div class="col-md-9">
-                <div class="card">
-                    <div class="card-header">unidad {{ $unidad->id }}</div>
-                    <div class="card-body">
+                    <a href="{{ route('unidad') }}" title="Back"><button class="btn btn-warning btn-sm"><i
+                                class="fa fa-arrow-left" aria-hidden="true"></i> Regresar</button></a>
+                    <a href="{{ route('edit_unidad',  $unidad->id) }}" title="Edit unidad"><button
+                            class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                            Editar</button></a>
 
-                        <a href="{{ url('/unidad') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-                        <a href="{{ url('/unidad/' . $unidad->id . '/edit') }}" title="Edit unidad"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                    <form method="POST" action="{{ route('destroy_unidad' , $unidad->id) }}" accept-charset="UTF-8"
+                        style="display:inline">
+                        {{ method_field('DELETE') }}
+                        {{ csrf_field() }}
+                        <button type="submit" class="btn btn-danger btn-sm" title="Delete unidad"
+                            onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o"
+                                aria-hidden="true"></i> Eliminar</button>
+                    </form>
+                    <br />
+                    <br />
 
-                        <form method="POST" action="{{ url('unidad' . '/' . $unidad->id) }}" accept-charset="UTF-8" style="display:inline">
-                            {{ method_field('DELETE') }}
-                            {{ csrf_field() }}
-                            <button type="submit" class="btn btn-danger btn-sm" title="Delete unidad" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
-                        </form>
-                        <br/>
-                        <br/>
-
-                        <div class="table-responsive">
-                            <table class="table">
-                                <tbody>
-                                    <tr>
-                                        <th>ID</th><td>{{ $unidad->id }}</td>
-                                    </tr>
-                                    <tr><th> Nombre </th><td> {{ $unidad->nombre }} </td></tr>
-                                </tbody>
-                            </table>
-                        </div>
-
+                    <div class="table-responsive">
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <th>ID</th>
+                                    <td>{{ $unidad->id }}</td>
+                                </tr>
+                                <tr>
+                                    <th> Nombre </th>
+                                    <td> {{ $unidad->nombre }} </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
+
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
