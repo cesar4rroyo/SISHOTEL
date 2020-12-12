e20842122f7260004d5750a0f324d49292050383
Cesar <cesardavid_09@hotmail.com>
2020-12-11 22:35:21 -0500

    update inicio's index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,59 +9,9 @@
     <div class="row">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">ADMIN PRINCIPAL</div>
+                <div class="card-header">INICIO</div>
                 <div class="card-body">
-                    <h3>Links</h3>
-                    <ul>
-                        <li>
-                            <a href="{{route('tipohabitacion')}}">tipohabitacion</a>
-                        </li>
-                        <li>
-                            <a href="{{route('piso')}}">piso</a>
-                        </li>
-                        <li>
-                            <a href="{{route('habitacion')}}">habitacion</a>
-                        </li>
-                        <li>
-                            <a href="{{route('concepto')}}">concepto</a>
-                        </li>
-                        <li>
-                            <a href="{{route('servicios')}}">servicios</a>
-                        </li>
-                        <li>
-                            <a href="{{route('categoria')}}">categoria</a>
-                        </li>
-                        <li>
-                            <a href="{{route('producto')}}">producto</a>
-                        </li>
-                        <li>
-                            <a href="{{route('unidad')}}">unidad</a>
-                        </li>
-                        <li>
-                            <a href="{{route('grupomenu')}}">grupomenu</a>
-                        </li>
-                        <li>
-                            <a href="{{route('opcionmenu')}}">opcionmenu</a>
-                        </li>
-                        <li>
-                            <a href="{{route('acceso')}}">acceso</a>
-                        </li>
-                        <li>
-                            <a href="{{route('tipousuario')}}">tipousuario</a>
-                        </li>
-                        <li>
-                            <a href="{{route('rol')}}">rol</a>
-                        </li>
-                        <li>
-                            <a href="{{route('rolpersona')}}">rolpersona</a>
-                        </li>
-                        <li>
-                            <a href="{{route('persona')}}">persona</a>
-                        </li>
-                        <li>
-                            <a href="{{route('nacionalidad')}}">nacionalidad</a>
-                        </li>
-                    </ul>
+                    Bienvenido, {{session()->get('usuario') ?? 'Invitado'}}
                 </div>
             </div>
         </div>
