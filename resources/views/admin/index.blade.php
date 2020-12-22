620b58c0f1905d5cf53e99a391394243e50dcefa
Cesar <cesardavid_09@hotmail.com>
2020-12-22 00:02:10 -0500

    diseño 1

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,12 +9,75 @@
     <div class="row">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">INICIO</div>
+                <div class="card-header text-uppercase">Bienvenido, {{session()->get('usuario') ?? 'Invitado'}}</div>
                 <div class="card-body">
-                    Bienvenido, {{session()->get('usuario') ?? 'Invitado'}}
+                    <div class="row">
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-warning">
+                                <div class="inner">
+                                    <h4 class="font-weight-bold">Personas</h4>
+                                    <br>
+                                    <br>
+                                </div>
+                                <div class="icon">
+                                    <i class="fas fa-user-plus"></i>
+                                </div>
+                                <a href="{{route('persona')}}" class="small-box-footer">
+                                    Mas información <i class="fas fa-arrow-circle-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h4 class="font-weight-bold">Productos</h4>
+                                    <br>
+                                    <br>
+                                </div>
+                                <div class="icon">
+                                    <i class="fas fa-gift"></i>
+                                </div>
+                                <a href="{{route('producto')}}" class="small-box-footer">
+                                    Más información<i class="fas fa-arrow-circle-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-success">
+                                <div class="inner">
+                                    <h4 class="font-weight-bold">Habitaciones</h4>
+                                    <br>
+                                    <br>
+                                </div>
+                                <div class="icon">
+                                    <i class="fas fa-hotel"></i>
+                                </div>
+                                <a href="{{route('habitacion')}}" class="small-box-footer">
+                                    Más información<i class="fas fa-arrow-circle-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-danger">
+                                <div class="inner">
+                                    <h4 class="font-weight-bold">Servicios</h4>
+                                    <br>
+                                    <br>
+                                </div>
+                                <div class="icon">
+                                    <i class="fas fa-server"></i>
+                                </div>
+                                <a href="{{route('servicios')}}" class="small-box-footer">
+                                    Más información<i class="fas fa-arrow-circle-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
         </div>
+
     </div>
 </div>
 @endsection
