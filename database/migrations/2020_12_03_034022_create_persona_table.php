f9915a0b2c4e84441ded1f92859f0948876494ec
Cesar <cesardavid_09@hotmail.com>
2021-01-01 20:46:30 -0500

    update migrations persona

--- database/migrations/2020_12_03_034022_create_persona_table.php
+++ database/migrations/2020_12_03_034022_create_persona_table.php
@@ -16,15 +16,18 @@ class CreatePersonaTable extends Migration
         Schema::create('persona', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombres', 50);
+            $table->integer('edad')->nullable();
             $table->string('apellidos', 50)->nullable();
             $table->string('razonsocial')->nullable();
             $table->string('ruc')->nullable();
-            $table->string('dni', 8)->nullable();
+            $table->string('dni')->nullable();
+            $table->string('ciudad')->nullable();
             $table->string('direccion', 100)->nullable();
             $table->string('sexo')->nullable();
             $table->date('fechanacimiento')->nullable();
             $table->string('telefono');
             $table->string('observacion', 500)->nullable();
+            $table->string('email', 50);
             $table->unsignedInteger('nacionalidad_id')->nullable();
             $table->foreign('nacionalidad_id', 'fk_persona_nacionalidad')
                 ->nullable()
